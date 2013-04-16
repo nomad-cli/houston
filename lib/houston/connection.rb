63d4103bab75af7693a0d99f98c1a40b2c52be2c
Mattt Thompson <m@mattt.me>
2013-04-16 12:30:06 +0200

    Alternative take on persistent connections

--- lib/houston/connection.rb
+++ lib/houston/connection.rb
@@ -1,32 +1,68 @@
 require 'uri'
 require 'socket'
 require 'openssl'
+require 'forwardable'
 
 module Houston
   class Connection
+    attr_reader :ssl, :socket, :host, :port, :certificate, :passphrase
+
+    extend Forwardable
+    def_delegators :@ssl, :read, :write
+
     class << self
       def open(options = {})
         return unless block_given?
 
-        [:certificate, :host, :port].each do |option|
-          raise ArgumentError, "Missing connection parameter: #{option}" unless options.has_key?(option)
-        end
+        connection = new(options)
+        connection.open
 
-        socket = TCPSocket.new(options[:host], options[:port])
+        yield connection
 
-        context = OpenSSL::SSL::SSLContext.new
-        context.key = OpenSSL::PKey::RSA.new(options[:certificate], options[:passphrase])
-        context.cert = OpenSSL::X509::Certificate.new(options[:certificate])
-
-        ssl = OpenSSL::SSL::SSLSocket.new(socket, context)
-        ssl.sync = true
-        ssl.connect
-
-        yield ssl, socket
-
-        ssl.close
-        socket.close
+        connection.close
       end
+    end
+
+    def initialize(options = {})
+      [:host, :port, :certificate].each do |option|
+        raise ArgumentError, "Missing connection parameter: #{option}" unless options.has_key?(option)
+      end
+
+      @host = options[:host]
+      @port = options[:port]
+      @certificate = options[:certificate]
+      @passphrase = options[:passphrase]
+    end
+
+    def open
+      return false if open?
+
+      @socket = TCPSocket.new(@host, @port)
+
+      context = OpenSSL::SSL::SSLContext.new
+      context.key = OpenSSL::PKey::RSA.new(@certificate, @passphrase)
+      context.cert = OpenSSL::X509::Certificate.new(@certificate)
+
+      @ssl = OpenSSL::SSL::SSLSocket.new(@socket, context)
+      ssl.sync = true
+    end
+
+    def open?
+      not (@ssl and @socket).nil?
+    end
+
+    def close
+      return false if closed?
+
+      @ssl.close
+      @ssl = nil
+
+      @socket.close
+      @socket = nil
+    end
+
+    def closed?
+      not open?
     end
   end
 end
