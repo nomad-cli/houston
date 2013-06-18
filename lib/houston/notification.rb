1ba83fd81be7e224debc96b4c00b0697e269cfdc
Joao Carlos <mail@joao-carlos.com>
2013-06-18 07:27:01 +0300

    Switch to enhanced notification format

--- lib/houston/notification.rb
+++ lib/houston/notification.rb
@@ -2,7 +2,7 @@ require 'json'
 
 module Houston
   class Notification
-    attr_accessor :token, :alert, :badge, :sound, :content_available, :custom_data
+    attr_accessor :token, :alert, :badge, :sound, :content_available, :custom_data, :id, :expiry
     attr_reader :sent_at
 
     alias :device :token
@@ -13,6 +13,8 @@ module Houston
       @alert = options.delete(:alert)
       @badge = options.delete(:badge)
       @sound = options.delete(:sound)
+      @expiry = options.delete(:expiry)
+      @id = options.delete(:id)
       @content_available = options.delete(:content_available)
 
       @custom_data = options
@@ -31,9 +33,11 @@ module Houston
 
     def message
       json = payload.to_json
-      hex_token = [@token.gsub(/[<\s>]/, '')].pack('H*')
+      device_token = [@token.gsub(/[<\s>]/, '')].pack('H*')
+      @expiry ||= Time.now + 86400
+      @id ||= 0
 
-      [0, 0, 32, hex_token, 0, json.bytes.count, json].pack('ccca*cca*')
+      [1, @id, @expiry.to_i, 0, 32, device_token, 0, json.bytes.count, json].pack('ciicca*cca*')
     end
 
     def mark_as_sent!
