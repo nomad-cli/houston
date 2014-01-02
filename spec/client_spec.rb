596c55d55e09c4426421ade81254795774b4ed3f
Greg Spiers <greg.spiers@shinydevelopment.com>
2014-01-02 16:58:12 +0000

    Clean up the specs, set unregistered devices instance variable directly, no need to pass it in to init.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 class MockConnection
   class << self
     def open(uri, certificate, passphrase)
-      yield self.new([[443779200, 32, "ce8be6272e43e85516033e24b4c289220eeda4879c477160b2545e95b68b5969"], [1388678223, 32, "ce8be6272e43e85516033e24b4c289220eeda4879c477160b2545e95b68b5970"]])
+      yield self.new
     end
   end
 
-  def initialize(unregistered_devices)
+  def initialize
     @read_count = 0
-    @unregistered_devices = unregistered_devices
+    @unregistered_devices = [[443779200, 32, "ce8be6272e43e85516033e24b4c289220eeda4879c477160b2545e95b68b5969"], [1388678223, 32, "ce8be6272e43e85516033e24b4c289220eeda4879c477160b2545e95b68b5970"]]
   end
 
   def read(bytes)
