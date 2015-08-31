642d34e213279d100e3032824c884ac6d3aa29d7
Michel Epsztejn <michel@seekingalpha.com>
2015-08-31 10:11:44 +0300

    fix bugs and use connection pool

--- lib/houston/client.rb
+++ lib/houston/client.rb
@@ -34,83 +34,122 @@ module Houston
       @timeout = Float(ENV['APN_TIMEOUT'] || 0.5)
     end
 
-    def push(*notifications)
-      return if notifications.empty?
+    def push(*notifications, &update_block)
+      @mutex = Mutex.new
+      @failed_notifications = []
+
+      beginning = Time.now
+
+      @connections = []
+      10.times{
+        connection = Connection.new(@gateway_uri, @certificate, @passphrase)
+        connection.open
+        @connections << connection
+      }
+      logger = Logger.new("michel_test.log", 'daily')
+      logger.info("Connections creation took: #{Time.now - beginning}")
+
+      beginning = Time.now
 
       notifications.flatten!
-      failed_notifications = []
+      error_index = send_notifications(notifications, &update_block)
+      while error_index > -1
+        notifications.shift(error_index + 1)
+        notifications.each{|n|n.mark_as_unsent!}
+        error_index = send_notifications(notifications, &update_block)
+      end
 
-      Connection.open(@gateway_uri, @certificate, @passphrase) do |connection|
-        ssl = connection.ssl
+      logger = Logger.new("michel_test.log", 'daily')
+      logger.info("finished after #{Time.now - beginning}")
 
-        error_index = -1
-        mutex = Mutex.new
-        Thread.abort_on_exception=true
+      @failed_notifications
+    end
 
-        read_thread = Thread.new do
-         begin
-           read_socket, write_socket, errors = IO.select([ssl], [], [ssl], nil)
-           if (read_socket && read_socket[0])
-             if error = connection.read(6)
-               command, status, index = error.unpack("ccN")
-               logger = Logger.new("houston_test.log", 'daily')
-               logger.error("error_at:#{Time.now.to_s}, error_code: #{status}, index_error: #{index}")
-               mutex.synchronize do
-                 error_index = index
-                 notifications[error_index].apns_error_code = status
-                 failed_notifications << notifications[error_index]
-               end
-             end
-           end
-         rescue
-           redo
-         end
-        end
+    def get_connection
+      Thread.new{
+        connection = Connection.new(@gateway_uri, @certificate, @passphrase)
+        connection.open
+        @connections << connection
+      }
+      @mutex.synchronize do
+        @connections.shift || Connection.new(@gateway_uri, @certificate, @passphrase)
+      end
+    end
 
-        write_thread = Thread.new do
-          notifications.each_with_index do |notification, index|
-            begin
-              last_time = Time.now
-              next unless notification.kind_of?(Notification)
-              next if notification.sent?
-              next unless notification.valid?
-              mutex.synchronize do
-                if error_index > -1
-                  connection.close
-                  Thread.exit
-                end
-              end
-              notification.id = index
-              connection.write(notification.message)
-              notification.mark_as_sent!
-              logger = Logger.new("houston_test.log", 'daily')
-              logger.info("sent_at:#{Time.now.to_s}, diff: #{Time.now - last_time}, token: #{notification.token}")
-            rescue => error
-              logger = Logger.new("houston_test.log", 'daily')
-              logger.error("#{error.inspect}, token: #{notification.token}")
-              mutex.synchronize do
-                error_index = index - 1
-              end
+    def send_notifications(*notifications, &update_block)
+      return if notifications.empty?
+      notifications.flatten!
+      error_index = -1
+
+      # Connection.open(@gateway_uri, @certificate, @passphrase) do |connection|
+      connection = get_connection
+      logger = Logger.new("michel_test.log", 'daily')
+      logger.info("get connection")
+
+      ssl = connection.ssl
+
+      Thread.abort_on_exception=true
+      read_thread = nil
+
+      write_thread = Thread.new do
+        notifications.each_with_index do |notification, index|
+          begin
+            last_time = Time.now
+            next unless notification.kind_of?(Notification)
+            next if notification.sent?
+            next unless notification.valid?
+            notification.id = index
+            connection.write(notification.message)
+            notification.mark_as_sent!
+            logger = Logger.new("michel_test.log", 'daily')
+            logger.info("sent_at:#{Time.now.to_s}, diff: #{Time.now - last_time}, token: #{notification.token}, text: #{notification.alert}")
+
+            if block_given? && (index % 200 == 0)
+              update_block.call(index)
+            end
+          rescue => error
+            logger = Logger.new("michel_test.log", 'daily')
+            logger.error("#{error.inspect}, token: #{notification.token}")
+            @mutex.synchronize do
+              error_index = index - 1
             end
           end
-          # sleep in order to receive last errors from apple in read thread
-          sleep(5)
+          # read_thread.exit if index == notifications.size - 1
         end
-
-        write_thread.join
+        # sleep in order to receive last errors from apple in read thread
+        # if regular_exit
+        logger = Logger.new("michel_test.log", 'daily')
+        logger.info("sleep")
+        sleep(2)
         read_thread.exit
+        # end
+      end
 
-        # start over with remaining notifications
-        if error_index > -1 && error_index < notifications.size - 1
-          notifications.shift(error_index + 1)
-          notifications.each{|n|n.mark_as_unsent!}
-          temp_connection = Houston::Connection.new(@gateway_uri, @certificate, @passphrase)
-          temp_connection.open
-          connection = temp_connection
+      read_thread = Thread.new do
+        begin
+          read_socket, write_socket, errors = IO.select([ssl], [], [ssl], nil)
+          if (read_socket && read_socket[0])
+            if error = connection.read(6)
+              command, status, index = error.unpack("ccN")
+              logger = Logger.new("michel_test.log", 'daily')
+              logger.error("error_at:#{Time.now.to_s}, error_code: #{status}, index_error: #{index}")
+              write_thread.exit
+              error_index = index
+              notifications[error_index].apns_error_code = status
+              @failed_notifications << notifications[error_index]
+              connection.close
+              read_thread.exit
+            end
+          end
+        rescue
           redo
         end
       end
-      failed_notifications
+
+      read_thread.join
+
+      # end
+      error_index
     end
 
     def unregistered_devices
